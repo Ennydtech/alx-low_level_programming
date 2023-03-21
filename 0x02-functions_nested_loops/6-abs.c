64490eb5428759f2d6691e6440ffdab2aee6cb74
ennydtech <eniolamuyiwaoni@gmail.com>
2023-03-21 14:13:39 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * int _abs - print postive or negative
- * @n: user input
+ * _abs - print postive or negative
+ * @a: user input
  * Return: Success (0) or (1)
  */
- int _abs(int a)
+int _abs(int a)
 {
 	if (a < 0)
 		a = -(a);
