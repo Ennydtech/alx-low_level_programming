c4f65126d624b5641d1398ed06acbe3f6de3eb5a
ennydtech <eniolamuyiwaoni@gmail.com>
2023-03-29 10:48:42 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,30 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
-
-/**
- * _strncpy - copies at most an inputted number
- * of bytes from string src into dest.
- * @dest: The buffer storing the string copy
- * @src: The source string.
- * @n: The maximum number of bytes to copied from src.
- * Return: A pointer to the resulting string dest.
- *
- */
-
-char *_strncpy(char *dest, char *src, int n)
-
+int main(void)
 {
-	int index = 0, src_len = 0;
+  int n;
+  int a[5];
+  int *p;
 
-	while (src[index++])
-	src_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-	dest[index] = src[index];
-
-	for (index = src_len; index < n; index++)
-	dest[index] = '\0';
-
-	return (dest);
-
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
